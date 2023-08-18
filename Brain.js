0694fbb0686a8160435de38c5a6384c7cf2564a2
Crawford Lieu <110504159+DefNotCrawf@users.noreply.github.com>
2023-08-18 11:40:43 +1000

    Clean up files, update html file

--- Brain.js
+++ Brain.js
@@ -1,19 +1,19 @@
 class AIAction {
-    constructor(isJump, holdTime, xDirection) {
-        this.isJump = isJump;
-        this.holdTime = holdTime;//number between 0 and 1
-        this.xDirection = xDirection;
+	constructor(isJump, holdTime, xDirection) {
+		this.isJump = isJump;
+		this.holdTime = holdTime;//number between 0 and 1
+		this.xDirection = xDirection;
 
-    }
+	}
 
-    clone() {
-        return new AIAction(this.isJump, this.holdTime, this.xDirection);
-    }
+	clone() {
+		return new AIAction(this.isJump, this.holdTime, this.xDirection);
+	}
 
-    mutate() {
-        this.holdTime += random(-0.3, 0.3);
-        this.holdTime = constrain(this.holdTime, 0.1, 1);
-    }
+	mutate() {
+		this.holdTime += random(-0.3, 0.3);
+		this.holdTime = constrain(this.holdTime, 0.1, 1);
+	}
 }
 
 
@@ -24,87 +24,87 @@ let chanceOfFullJump = 0.2;
 
 class Brain {
 
-    constructor(size, randomiseInstructions = true) {
-        this.instructions = [];
-        this.currentInstructionNumber = 0;
-        if (randomiseInstructions)
-            this.randomize(size);
-        this.parentReachedBestLevelAtActionNo = 0;
-    }
+	constructor(size, randomiseInstructions = true) {
+		this.instructions = [];
+		this.currentInstructionNumber = 0;
+		if (randomiseInstructions)
+			this.randomize(size);
+		this.parentReachedBestLevelAtActionNo = 0;
+	}
 
-    randomize(size) {
-        for (let i = 0; i < size; i++) {
-            this.instructions[i] = this.getRandomAction();
-        }
-    }
+	randomize(size) {
+		for (let i = 0; i < size; i++) {
+			this.instructions[i] = this.getRandomAction();
+		}
+	}
 
-    getRandomAction() {
-        let isJump = false;
+	getRandomAction() {
+		let isJump = false;
 
-        if (random() > jumpChance) {
-            isJump = true;
-        }
+		if (random() > jumpChance) {
+			isJump = true;
+		}
 
-        let holdTime = random(0.1, 1);
-        if (random() < chanceOfFullJump) {
-            holdTime = 1;
-        }
-
-
-        let directions = [-1, -1, -1, 0, 1, 1, 1]
-        let xDirection = random(directions)
+		let holdTime = random(0.1, 1);
+		if (random() < chanceOfFullJump) {
+			holdTime = 1;
+		}
 
 
-        return new AIAction(isJump, holdTime, xDirection)
-    }
-
-    getNextAction() {
-        if (this.currentInstructionNumber >= this.instructions.length) {
-            return null;
-        }
-        this.currentInstructionNumber += 1;
-        return this.instructions[this.currentInstructionNumber - 1];
-    }
+		let directions = [-1, -1, -1, 0, 1, 1, 1]
+		let xDirection = random(directions)
 
 
-    clone() {
-        let clone = new Brain(this.size, false);
-        clone.instructions = [];
-        for (let i = 0; i < this.instructions.length; i++) {
-            clone.instructions.push(this.instructions[i].clone())
-        }
-        return clone;
-    }
+		return new AIAction(isJump, holdTime, xDirection)
+	}
 
-    mutate() {
-        let mutationRate = 0.1;
-        let chanceOfNewInstruction = 0.02;
-        for (let i = this.parentReachedBestLevelAtActionNo; i < this.instructions.length; i++) {
-            if (random() < chanceOfNewInstruction) {
-                this.instructions[i] = this.getRandomAction()
-            } else if (random() < mutationRate) {
-                this.instructions[i].mutate();
-            }
-        }
-    }
+	getNextAction() {
+		if (this.currentInstructionNumber >= this.instructions.length) {
+			return null;
+		}
+		this.currentInstructionNumber += 1;
+		return this.instructions[this.currentInstructionNumber - 1];
+	}
 
-    mutateActionNumber(actionNumber) {
-        // let mutationRate = 0.1;
 
-        actionNumber -= 1; // this is done because im a bad programmer
-        let chanceOfNewInstruction = 0.2;
-        if (random() < chanceOfNewInstruction) {
-            this.instructions[actionNumber] = this.getRandomAction()
-        } else {
-            this.instructions[actionNumber].mutate();
-        }
-    }
+	clone() {
+		let clone = new Brain(this.size, false);
+		clone.instructions = [];
+		for (let i = 0; i < this.instructions.length; i++) {
+			clone.instructions.push(this.instructions[i].clone())
+		}
+		return clone;
+	}
 
-    increaseMoves(increaseMovesBy) {
-        for (var i = 0; i < increaseMovesBy; i++) {
-            this.instructions.push(this.getRandomAction());
-        }
+	mutate() {
+		let mutationRate = 0.1;
+		let chanceOfNewInstruction = 0.02;
+		for (let i = this.parentReachedBestLevelAtActionNo; i < this.instructions.length; i++) {
+			if (random() < chanceOfNewInstruction) {
+				this.instructions[i] = this.getRandomAction()
+			} else if (random() < mutationRate) {
+				this.instructions[i].mutate();
+			}
+		}
+	}
 
-    }
+	mutateActionNumber(actionNumber) {
+		// let mutationRate = 0.1;
+
+		actionNumber -= 1; // this is done because im a bad programmer
+		let chanceOfNewInstruction = 0.2;
+		if (random() < chanceOfNewInstruction) {
+			this.instructions[actionNumber] = this.getRandomAction()
+		} else {
+			this.instructions[actionNumber].mutate();
+		}
+	}
+
+	increaseMoves(increaseMovesBy) {
+		for (var i = 0; i < increaseMovesBy; i++) {
+			this.instructions.push(this.getRandomAction());
+		}
+
+	}
 
 }
--- sketch.js
+++ sketch.js
@@ -48,153 +48,153 @@ let evolationSpeed = 1;
 
 
 function preload() {
-    backgroundImage = loadImage('images/levelImages/1.png')
-    idleImage = loadImage('images/poses/idle.png')
-    squatImage = loadImage('images/poses/squat.png')
-    jumpImage = loadImage('images/poses/jump.png')
-    oofImage = loadImage('images/poses/oof.png')
-    run1Image = loadImage('images/poses/run1.png')
-    run2Image = loadImage('images/poses/run2.png')
-    run3Image = loadImage('images/poses/run3.png')
-    fallenImage = loadImage('images/poses/fallen.png')
-    fallImage = loadImage('images/poses/fall.png')
+	backgroundImage = loadImage('images/levelImages/1.png')
+	idleImage = loadImage('images/poses/idle.png')
+	squatImage = loadImage('images/poses/squat.png')
+	jumpImage = loadImage('images/poses/jump.png')
+	oofImage = loadImage('images/poses/oof.png')
+	run1Image = loadImage('images/poses/run1.png')
+	run2Image = loadImage('images/poses/run2.png')
+	run3Image = loadImage('images/poses/run3.png')
+	fallenImage = loadImage('images/poses/fallen.png')
+	fallImage = loadImage('images/poses/fall.png')
 
 
-    snowImage = loadImage('images/snow3.png')
+	snowImage = loadImage('images/snow3.png')
 
-    for (let i = 1; i <= 43; i++) {
-        levelImages.push(loadImage('images/levelImages/' + i + '.png'))
-    }
+	for (let i = 1; i <= 43; i++) {
+		levelImages.push(loadImage('images/levelImages/' + i + '.png'))
+	}
 
-    jumpSound = loadSound('sounds/jump.mp3')
-    fallSound = loadSound('sounds/fall.mp3')
-    bumpSound = loadSound('sounds/bump.mp3')
-    landSound = loadSound('sounds/land.mp3')
+	jumpSound = loadSound('sounds/jump.mp3')
+	fallSound = loadSound('sounds/fall.mp3')
+	bumpSound = loadSound('sounds/bump.mp3')
+	landSound = loadSound('sounds/land.mp3')
 
 
 }
 
 
 function setup() {
-    setupCanvas();
-    player = new Player();
-    population = new Population(600);
-    setupLevels();
-    jumpSound.playMode('sustain');
-    fallSound.playMode('sustain');
-    bumpSound.playMode('sustain');
-    landSound.playMode('sustain');
+	setupCanvas();
+	player = new Player();
+	population = new Population(600);
+	setupLevels();
+	jumpSound.playMode('sustain');
+	fallSound.playMode('sustain');
+	bumpSound.playMode('sustain');
+	landSound.playMode('sustain');
 
-    // lines.push(new Line(200,height - 80,width - 200, height-80));
-    // lines.push(new Line(10,height - 500,200, height-500));
-    // lines.push(new Line(200,height - 100,200, height-500));
+	// lines.push(new Line(200,height - 80,width - 200, height-80));
+	// lines.push(new Line(10,height - 500,200, height-500));
+	// lines.push(new Line(200,height - 100,200, height-500));
 
 
 }
 
 function drawMousePosition() {
-    let snappedX = mouseX - mouseX % 20;
-    let snappedY = mouseY - mouseY % 20;
-    push();
+	let snappedX = mouseX - mouseX % 20;
+	let snappedY = mouseY - mouseY % 20;
+	push();
 
 
-    fill(255, 0, 0)
-    noStroke();
-    ellipse(snappedX, snappedY, 5);
+	fill(255, 0, 0)
+	noStroke();
+	ellipse(snappedX, snappedY, 5);
 
-    if (mousePos1 != null) {
-        stroke(255, 0, 0)
-        strokeWeight(5)
-        line(mousePos1.x, mousePos1.y, snappedX, snappedY)
-    }
+	if (mousePos1 != null) {
+		stroke(255, 0, 0)
+		strokeWeight(5)
+		line(mousePos1.x, mousePos1.y, snappedX, snappedY)
+	}
 
-    pop();
+	pop();
 }
 
 let levelNumber = 0;
 
 function draw() {
-    background(10);
+	background(10);
 
 
-    // if(frameCount % 5==0 ){
-    //
-    //     levelNumber  = (levelNumber +1)%43;
-    // }
-    // image(backgroundImage,0,0);
-    // if (!creatingLines) {
+	// if(frameCount % 5==0 ){
+	//
+	//     levelNumber  = (levelNumber +1)%43;
+	// }
+	// image(backgroundImage,0,0);
+	// if (!creatingLines) {
 
-    //     if (!placingPlayer || playerPlaced) {
-    //
-    //         player.Update();
-    //         player.Show();
-    //     }
-    // } else {
-    //     image(levelImages[levelNumber], 0, 0)
-    // }
-    push()
-    translate(0, 50);
-    if (testingSinglePlayer) {
-        image(levels[player.currentLevelNo].levelImage, 0, 0)
-        levels[player.currentLevelNo].show();
-        player.Update();
-        player.Show();
-    } else if(replayingBestPlayer) {
-        if(!cloneOfBestPlayer.hasFinishedInstructions){
-            for (let i = 0; i < evolationSpeed; i++){
-                cloneOfBestPlayer.Update()
-            }
+	//     if (!placingPlayer || playerPlaced) {
+	//
+	//         player.Update();
+	//         player.Show();
+	//     }
+	// } else {
+	//     image(levelImages[levelNumber], 0, 0)
+	// }
+	push()
+	translate(0, 50);
+	if (testingSinglePlayer) {
+		image(levels[player.currentLevelNo].levelImage, 0, 0)
+		levels[player.currentLevelNo].show();
+		player.Update();
+		player.Show();
+	} else if(replayingBestPlayer) {
+		if(!cloneOfBestPlayer.hasFinishedInstructions){
+			for (let i = 0; i < evolationSpeed; i++){
+				cloneOfBestPlayer.Update()
+			}
 
-            showLevel(cloneOfBestPlayer.currentLevelNo);
-            alreadyShowingSnow = false;
-            cloneOfBestPlayer.Show();
-        }else{
-            replayingBestPlayer = false;
-            mutePlayers = true;
-        }
+			showLevel(cloneOfBestPlayer.currentLevelNo);
+			alreadyShowingSnow = false;
+			cloneOfBestPlayer.Show();
+		}else{
+			replayingBestPlayer = false;
+			mutePlayers = true;
+		}
 
-    }else{
+	}else{
 
-        if (population.AllPlayersFinished()) {
-            population.NaturalSelection();
-            if (population.gen % increaseActionsEveryXGenerations === 0) {
-                population.IncreasePlayerMoves(increaseActionsByAmount);
-            }
-        }
-        for (let i = 0; i < evolationSpeed; i++)
-            population.Update()
-        // population.Update()
-        // population.Update()
-        population.Show();
+		if (population.AllPlayersFinished()) {
+			population.NaturalSelection();
+			if (population.gen % increaseActionsEveryXGenerations === 0) {
+				population.IncreasePlayerMoves(increaseActionsByAmount);
+			}
+		}
+		for (let i = 0; i < evolationSpeed; i++)
+			population.Update()
+		// population.Update()
+		// population.Update()
+		population.Show();
 
-    }
-
-
-    if (showingLines || creatingLines)
-        showLines();
-
-    if (creatingLines)
-        drawMousePosition();
+	}
 
 
-    if (frameCount % 15 === 0) {
-        previousFrameRate = floor(getFrameRate())
-    }
+	if (showingLines || creatingLines)
+		showLines();
+
+	if (creatingLines)
+		drawMousePosition();
 
 
-    pop();
+	if (frameCount % 15 === 0) {
+		previousFrameRate = floor(getFrameRate())
+	}
 
-    fill(0);
-    noStroke();
-    rect(0, 0, width, 50);
-    if(!testingSinglePlayer){
-        textSize(32);
-        fill(255, 255, 255);
-        text('FPS: ' + previousFrameRate, width - 160, 35);
-        text('Gen: ' + population.gen, 30, 35);
-        text('Moves: ' + population.players[0].brain.instructions.length, 200, 35);
-        text('Best Height: ' + population.bestHeight, 400, 35);
-    }
+
+	pop();
+
+	fill(0);
+	noStroke();
+	rect(0, 0, width, 50);
+	if(!testingSinglePlayer){
+		textSize(32);
+		fill(255, 255, 255);
+		text('FPS: ' + previousFrameRate, width - 160, 35);
+		text('Gen: ' + population.gen, 30, 35);
+		text('Moves: ' + population.players[0].brain.instructions.length, 200, 35);
+		text('Best Height: ' + population.bestHeight, 400, 35);
+	}
 
 
 }
@@ -202,59 +202,59 @@ function draw() {
 let previousFrameRate = 60;
 
 function showLevel(levelNumberToShow) {
-    // print(levelNumberToShow)
-    // image(levels[levelNumberToShow].levelImage, 0, 0)
-    levels[levelNumberToShow].show();
+	// print(levelNumberToShow)
+	// image(levels[levelNumberToShow].levelImage, 0, 0)
+	levels[levelNumberToShow].show();
 }
 
 function showLines() {
-    if (creatingLines) {
-        for (let l of lines) {
-            l.Show();
-        }
-    } else {
+	if (creatingLines) {
+		for (let l of lines) {
+			l.Show();
+		}
+	} else {
 
-        for (let l of levels[player.currentLevelNo].lines) {
-            l.Show();
-        }
+		for (let l of levels[player.currentLevelNo].lines) {
+			l.Show();
+		}
 
-    }
+	}
 
 }
 
 
 function setupCanvas() {
-    canvas = createCanvas(1200, 950);
-    canvas.parent('canvas');
-    width = canvas.width;
-    height = canvas.height - 50;
+	canvas = createCanvas(1200, 950);
+	canvas.parent('canvas');
+	width = canvas.width;
+	height = canvas.height - 50;
 }
 
 
 function keyPressed() {
-    switch (key) {
-        case ' ':
-            player.jumpHeld = true
-            break;
-        case 'R':
-            population.ResetAllPlayers()
-            break;
-        case 'S':
-            bumpSound.stop();
-            jumpSound.stop();
-            landSound.stop();
-            fallSound.stop();
-            break;
-    }
+	switch (key) {
+		case ' ':
+			player.jumpHeld = true
+			break;
+		case 'R':
+			population.ResetAllPlayers()
+			break;
+		case 'S':
+			bumpSound.stop();
+			jumpSound.stop();
+			landSound.stop();
+			fallSound.stop();
+			break;
+	}
 
-    switch (keyCode) {
-        case LEFT_ARROW:
-            player.leftHeld = true;
-            break;
-        case RIGHT_ARROW:
-            player.rightHeld = true;
-            break;
-    }
+	switch (keyCode) {
+		case LEFT_ARROW:
+			player.leftHeld = true;
+			break;
+		case RIGHT_ARROW:
+			player.rightHeld = true;
+			break;
+	}
 
 }
 replayingBestPlayer = false;
@@ -264,70 +264,70 @@ cloneOfBestPlayer = null;
 
 function keyReleased() {
 
-    switch (key) {
-        case 'B':
-            replayingBestPlayer = true;
-            cloneOfBestPlayer = population.cloneOfBestPlayerFromPreviousGeneration.clone();
-            evolationSpeed = 1;
-            mutePlayers = false;
-            break;
+	switch (key) {
+		case 'B':
+			replayingBestPlayer = true;
+			cloneOfBestPlayer = population.cloneOfBestPlayerFromPreviousGeneration.clone();
+			evolationSpeed = 1;
+			mutePlayers = false;
+			break;
 
 
-        case ' ':
+		case ' ':
 
-            if (!creatingLines) {
-                player.jumpHeld = false
-                player.Jump()
-            }
-            break;
-        case 'R':
-            if (creatingLines) {
-                lines = [];
-                linesString = "";
-                mousePos1 = null;
-                mousePos2 = null;
-            }
-            break;
-        case 'N':
-            if (creatingLines) {
-                levelNumber += 1;
-                linesString += '\nlevels.push(tempLevel);';
-                linesString += '\ntempLevel = new Level();';
-                print(linesString);
-                lines = [];
-                linesString = '';
-                mousePos1 = null;
-                mousePos2 = null;
-            } else {
-                player.currentLevelNo += 1;
-                print(player.currentLevelNo);
-            }
-            break;
-        case 'D':
-            if (creatingLines) {
+			if (!creatingLines) {
+				player.jumpHeld = false
+				player.Jump()
+			}
+			break;
+		case 'R':
+			if (creatingLines) {
+				lines = [];
+				linesString = "";
+				mousePos1 = null;
+				mousePos2 = null;
+			}
+			break;
+		case 'N':
+			if (creatingLines) {
+				levelNumber += 1;
+				linesString += '\nlevels.push(tempLevel);';
+				linesString += '\ntempLevel = new Level();';
+				print(linesString);
+				lines = [];
+				linesString = '';
+				mousePos1 = null;
+				mousePos2 = null;
+			} else {
+				player.currentLevelNo += 1;
+				print(player.currentLevelNo);
+			}
+			break;
+		case 'D':
+			if (creatingLines) {
 
-                mousePos1 = null;
-                mousePos2 = null;
-            }
-    }
+				mousePos1 = null;
+				mousePos2 = null;
+			}
+	}
 
-    switch (keyCode) {
-        case LEFT_ARROW:
-            player.leftHeld = false;
-            break;
-        case RIGHT_ARROW:
-            player.rightHeld = false;
-            break;
-        case DOWN_ARROW:
-            evolationSpeed = constrain(evolationSpeed - 1, 0, 50);
-            print(evolationSpeed)
+	switch (keyCode) {
+		case LEFT_ARROW:
+			player.leftHeld = false;
+			break;
+		case RIGHT_ARROW:
+			player.rightHeld = false;
+			break;
+		case DOWN_ARROW:
+			evolationSpeed = constrain(evolationSpeed - 1, 0, 50);
+			print(evolationSpeed)
 
-            break;
-        case UP_ARROW:
-            evolationSpeed = constrain(evolationSpeed + 1, 0, 50);
-            print(evolationSpeed)
-            break;
-    }
+			break;
+		case UP_ARROW:
+			evolationSpeed = constrain(evolationSpeed + 1, 0, 50);
+			print(evolationSpeed)
+			break;
+	}
 }
 
 
@@ -337,29 +337,29 @@ let linesString = "";
 
 
 function mouseClicked() {
-    if (creatingLines) {
-        let snappedX = mouseX - mouseX % 20;
-        let snappedY = mouseY - mouseY % 20;
-        if (mousePos1 == null) {
-            mousePos1 = createVector(snappedX, snappedY);
-        } else {
-            mousePos2 = createVector(snappedX, snappedY);
-            // print('tempLevel.lines.push(new Line(' + mousePos1.x + ',' + mousePos1.y + ',' + mousePos2.x + ',' + mousePos2.y + '));');
-            lines.push(new Line(mousePos1.x, mousePos1.y, mousePos2.x, mousePos2.y));
-            linesString += '\ntempLevel.lines.push(new Line(' + mousePos1.x + ',' + mousePos1.y + ',' + mousePos2.x + ',' + mousePos2.y + '));';
-            mousePos1 = null;
-            mousePos2 = null;
-        }
-    } else if (placingPlayer && !playerPlaced) {
-        playerPlaced = true;
-        player.currentPos = createVector(mouseX, mouseY);
+	if (creatingLines) {
+		let snappedX = mouseX - mouseX % 20;
+		let snappedY = mouseY - mouseY % 20;
+		if (mousePos1 == null) {
+			mousePos1 = createVector(snappedX, snappedY);
+		} else {
+			mousePos2 = createVector(snappedX, snappedY);
+			// print('tempLevel.lines.push(new Line(' + mousePos1.x + ',' + mousePos1.y + ',' + mousePos2.x + ',' + mousePos2.y + '));');
+			lines.push(new Line(mousePos1.x, mousePos1.y, mousePos2.x, mousePos2.y));
+			linesString += '\ntempLevel.lines.push(new Line(' + mousePos1.x + ',' + mousePos1.y + ',' + mousePos2.x + ',' + mousePos2.y + '));';
+			mousePos1 = null;
+			mousePos2 = null;
+		}
+	} else if (placingPlayer && !playerPlaced) {
+		playerPlaced = true;
+		player.currentPos = createVector(mouseX, mouseY);
 
 
-    } else if (placingCoins) {
+	} else if (placingCoins) {
 
 
-    }
-    print("levels[" + player.currentLevelNo + "].coins.push(new Coin( " + floor(mouseX) + "," + floor(mouseY - 50) + ' , "progress" ));');
+	}
+	print("levels[" + player.currentLevelNo + "].coins.push(new Coin( " + floor(mouseX) + "," + floor(mouseY - 50) + ' , "progress" ));');
 }
 
 //todo
